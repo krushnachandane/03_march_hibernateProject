5874940b80bb811207b0665c0359d25811b491a9
shubhamkrmahto <skm9258@gmail.com>
2025-03-03 12:54:30 +0530

    New Branch created
    added Update and delete methods

--- StudentManagementSystem/src/main/java/com/app/client/Test.java
+++ StudentManagementSystem/src/main/java/com/app/client/Test.java
@@ -2,12 +2,19 @@ package com.app.client;
 
 import java.util.Scanner;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.app.config.HibernateUtil;
+import com.app.model.Student;
+
 public class Test {
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		
 		while(true)
 		{
@@ -26,8 +33,34 @@ public class Test {
 			case 2:
 					break;
 			case 3:
+				Session session3 = sessionFactory.openSession();
+				System.out.println("Enter the student id to update");
+				
+				int id3 = sc.nextInt();
+				
+				Student s = session3.get(Student.class, id3);
+				
+				System.out.println("Enter the Student Name = ");
+				s.setName(sc.next()+sc.nextLine());
+				System.out.println("Enter the Student Mobile Number");
+				s.setMobno(sc.nextLong());
+				
+				session3.merge(s);
+				session3.beginTransaction().commit();
 					break;
+					
 			case 4:
+				Session session4 = sessionFactory.openSession();
+				
+				System.out.println("Enter the student id to delete");
+				
+				int id4 = sc.nextInt();
+				
+				Student s4 = session4.get(Student.class, id4); 
+				
+				session4.remove(s4);
+				
+				session4.beginTransaction().commit();
 					break;
 			default:
 					break;
